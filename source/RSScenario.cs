d88c772e5f9a687c121091155e60825645dbdde3
DMagic <david.grandy@gmail.com>
2015-07-29 00:33:27 -0400

    Basic scenario module from dev branch

---
+++ source/RSScenario.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using UnityEngine;
+using KSPPluginFramework;
+
+namespace RealScience
+{
+	[KSPScenario(ScenarioCreationOptions.AddToAllGames, new GameScenes[] { GameScenes.FLIGHT, GameScenes.EDITOR, GameScenes.SPACECENTER, GameScenes.TRACKSTATION })]
+	public class RSScenario : ScenarioModule
+	{
+		public static RSScenario Instance { get; private set; }
+		public bool isReady = false;
+
+		internal void Log(string message)
+		{
+			bool debug = true;
+			if (debug)
+				Debug.Log("[RealScience] " + message);
+		}
+
+		// KSP Methods
+		// ScenarioModule Methods
+		public override void OnAwake()
+		{
+			Instance = this;
+		}
+
+		public void Start()
+		{
+			Log("Scenario Start");
+			isReady = true;
+		}
+
+		public override void OnLoad(ConfigNode node)
+		{
+
+		}
+
+		public override void OnSave(ConfigNode node)
+		{
+
+		}
+	}
+}
